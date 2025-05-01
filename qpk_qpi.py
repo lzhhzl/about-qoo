e3f6c735d9fc49b5a9979a10e0f46e49b36ba4e9
lzhhzl <lzh68291@126.com>
2025-05-01 12:19:26 +0800

    init files

---
+++ qpk_qpi.py
@@ -0,0 +1,82 @@
+import os
+import struct
+import zlib
+
+def main():
+    # 设置当前工作目录为脚本所在的目录
+    current_script_path = r"D:\Program\qoo"
+    os.chdir(current_script_path)
+    
+    file = "KS"
+    qpi_path = file + ".QPI"
+    qpk_path = file + ".QPK"
+    
+    with open(qpi_path, 'rb') as qpi:
+        br = qpi.read()
+        
+        # 读取文件头（4 字节）
+        header = br[:4]
+        print(header.decode('ascii'))
+        
+        # 读取文件数量（4 字节）
+        count = struct.unpack('<I', br[4:8])[0]  # 估计不是正确的数量
+        count = 6  # 这里依据HKT中记载的文件数量自行设置
+        print(f"File Count: {count}")
+        
+        # 0x800 Alignment(QPK file start-offset)
+        br = br[0x1C:]  # 这里应该是从QPI记载着偏移量0x800(小端位00800000)的开始读取
+        
+        with open(qpk_path, 'rb') as qpk:
+            br2 = qpk.read()
+            
+            for i in range(count):
+                # 读取偏移量（4 字节）
+                offset = struct.unpack('<I', br[:4])[0]
+                br = br[4:]
+                # 读取大小（4 字节）
+                size = struct.unpack('<I', br[:4])[0]
+                br = br[4:]
+                # 其他参数
+                isCZL = False
+                ext = ".qfi"
+                
+                if (size & 0x80000000) >> 31 == 1:  # Empty File空文件
+                    print("Empty File")
+                elif (size & 0x40000000) >> 30 == 1:  # Compressed File压缩文件
+                    if br2[offset:offset+4].decode('ascii') == "CZL\x00":
+                        ext = ".czl"
+                        isCZL = True
+                else:  # 尝试获取 RIFF 头部
+                    if br2[offset:offset+4].decode('ascii') == "RIFF":
+                        ext = ".at3"
+                
+                print(f"{i:04d}{ext}")
+                print(f"Offset: {offset:02X}")
+                print(f"Size: {(size & 0x3FFFFFFF):02X}",end='\n\n')
+                
+                # 创建输出目录（如果不存在）
+                output_dir = "output"
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+                
+                output_path = os.path.join(output_dir, f"{i:04d}{ext}")
+                
+                with open(output_path, 'wb') as bw:
+                    if isCZL:
+                        # 读取压缩数据的大小（4 字节）
+                        size = struct.unpack('<I',br2[offset+0x4:offset+0x4+4])[0]
+                        cSize = (size & 0x3FFFFFFF)
+                        size += 0xC
+                        
+                        # 解压缩 CZL 文件
+                        decompressed_data = zlib.decompress(br2[offset+0xC:offset+0xC+cSize])
+                        
+                        # 保存解压缩后的数据
+                        unc_output_path = os.path.join(output_dir, f"{i:04d}{ext}.unc")
+                        with open(unc_output_path, 'wb') as bw2:
+                            bw2.write(decompressed_data)
+                    # 写入普通文件数据
+                    bw.write(br2[offset:offset+int(size & 0x3FFFFFFF)])
+
+if __name__ == "__main__":
+    main()
